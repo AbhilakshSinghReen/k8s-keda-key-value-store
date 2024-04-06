1096ffa840a0a27a44cfcd8d2df841756b98e379
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-04-06 17:24:06 +0530

    Added an optional delay to the tasks for testing KEDA.

--- server-worker/src/tasks.py
+++ server-worker/src/tasks.py
@@ -1,4 +1,5 @@
 from os import environ
+from time import sleep
 
 from huey import RedisHuey
 
@@ -6,6 +7,7 @@ from .redis_client import redis_client
 
 
 redis_host = environ["REDIS_HOST"]
+task_delay = float(environ.get("TASK_DELAY", 0))
 
 huey = RedisHuey('entrypoint', host=redis_host)
 
@@ -13,8 +15,10 @@ huey = RedisHuey('entrypoint', host=redis_host)
 @huey.task()
 def set_key_value_pair(key: str, value: str):
     redis_client.set(key, value)
-
+    sleep(task_delay)
+    
 
 @huey.task()
 def delete_key_value_pair(key: str):
     redis_client.delete(key)
+    sleep(task_delay)
